cd0f49ba162c0cdbb5a60c6e5f6b33eac2358ee6
Loic Raucy <lraucy@multiposting.fr>
2015-01-28 14:49:58 +0100

    Added widget for displaying upgrade available.

---
+++ .config/awesome/checkupgradewidget.lua
@@ -0,0 +1,25 @@
+local wibox = require("wibox")
+local vicious = require("vicious")
+
+function os.capture(cmd, raw)
+    local f = assert(io.popen(cmd, 'r'))
+    local s = assert(f:read('*a'))
+    f:close()
+    if raw then return s end
+    s = string.gsub(s, '^%s+', '')
+    s = string.gsub(s, '%s+$', '')
+    s = string.gsub(s, '[\n\r]+', ' ')
+    return s
+end
+
+local function get_updates(format, warg)
+    local _checkupgrade = {}
+    _checkupgrade.pacman = os.capture('pacman -Qqu | wc -l')
+    _checkupgrade.aur = os.capture('cower -u | wc -l')
+
+    return {_checkupgrade.pacman, _checkupgrade.aur}
+end
+
+checkupgradewidget = wibox.widget.textbox()
+vicious.register(checkupgradewidget, get_updates, "pacman: $1 AUR: $2 ", 600)
+return checkupgradewidget
